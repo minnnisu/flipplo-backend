f6276319d7840ea0fa33d692151edfa6fadfce22
CHOI-MIN-SU <mine52100@naver.com>
2024-10-07 11:46:22 +0900

    init: 프로젝트 생성

---
+++ src/main/java/org/flipplo/flipploService/FlipploServiceApplication.java
@@ -0,0 +1,13 @@
+package org.flipplo.flipploService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipploServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlipploServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/flipplo/flipploService/FlipploServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.flipplo.flipploService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipploServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
